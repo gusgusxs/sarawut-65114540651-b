beab82e8bf2bcfc0315153975611cfbeced25f16
gusgusxs <sarawut.ma.65@ubu.ac.th>
2025-03-02 10:55:09 +0700

    Delete B directory

--- B/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class coruse (models.Model):
-    id = models.IntegerField(unique=True,primary_key=True)
-    name = models.IntegerField(max_length=100)
-    info = models.IntegerField(max_length=100)
--- B/myapp/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import * 
-urlpatterns = [
-    path('show',showinfo.as_view(),name="show")
-]
